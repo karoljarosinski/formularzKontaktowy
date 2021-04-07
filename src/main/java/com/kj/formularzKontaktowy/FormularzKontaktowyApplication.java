3bdcabd8ecc05d8b998527894388384369048b9e
karol <karol.jarosinski91@gmail.com>
2021-04-07 18:19:22 +0200

    master

---
+++ src/main/java/com/kj/formularzKontaktowy/FormularzKontaktowyApplication.java
@@ -0,0 +1,13 @@
+package com.kj.formularzKontaktowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormularzKontaktowyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormularzKontaktowyApplication.class, args);
+    }
+
+}
